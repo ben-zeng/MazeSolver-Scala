374901bd14de69d2ee3f99ca84fa6b8b9e8a3a12
Ben Zeng <benzeng94@me.com>
2019-11-18 14:28:24 +0000

    spiked text reader

--- src/main/scala/MazeReader.scala
+++ src/main/scala/MazeReader.scala
@@ -1,4 +1,9 @@
-object MazeReader {
+import scala.io.Source
 
-
+object MazeReader extends App {
+  val mazeFile = Source.fromFile("test_mazes/medium_input.txt")
+  for (line <- mazeFile.getLines) {
+    println(line)
+  }
+  mazeFile.close
 }
